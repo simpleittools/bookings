5ea4d1951aa180cb48b3b64ed5725bbffbb66d42
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-08 19:15:12 -0800

    Add validation functionality for Min Length and add loop to check for all required form items

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -115,7 +115,8 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, r)
 	if !form.Valid() {
 		data := make(map[string]interface{})
 		data["reservation"] = reservation
@@ -130,10 +131,16 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// MakeReservation will render the reservation for the GET of the page and display the form
 func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(
 		w, r, "make-reservation.page.tmpl", &models.TemplateData{
 			Form: forms.New(nil),
+			Data: data,
 		},
 	)
 }
