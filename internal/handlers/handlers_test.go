9f5e1af45b9c939ecfb0ecf04dfd4ffb8b40c89f
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-13 15:08:39 -0800

    setup render tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -25,6 +26,26 @@ var theTests = []struct {
 	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{
+		"post-search-avail", "/search-availability", "POST", []postData{
+			{key: "start", value: "2023-01-01"},
+			{key: "end", value: "2023-01-02"},
+		}, http.StatusOK,
+	},
+	{
+		"post-search-avail-json", "/search-availability-json", "POST", []postData{
+			{key: "start", value: "2023-01-01"},
+			{key: "end", value: "2023-01-02"},
+		}, http.StatusOK,
+	},
+	{
+		"make-reservation-post", "/make-reservation", "POST", []postData{
+			{key: "first_name", value: "John"},
+			{key: "last_name", value: "Doe"},
+			{key: "email", value: "you@there.com"},
+			{key: "phone", value: "555-555-5555"},
+		}, http.StatusOK,
+	},
 }
 
 func TestNewHandlers(t *testing.T) {
@@ -43,7 +64,18 @@ func TestNewHandlers(t *testing.T) {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
-
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
---
+++ internal/render/setup_test.go
@@ -0,0 +1,52 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"github.com/simpleittools/bookings/internal/config"
+	"github.com/simpleittools/bookings/internal/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change to true when in production
+	testApp.InProduction = false
+
+	// initiate sessions
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
+
+type myWriter struct{}
+
+func (tw *myWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (tw *myWriter) WriteHeader(i int) {
+
+}
+
+func (tw *myWriter) Write(b []byte) (int, error) {
+	length := len(b)
+	return length, nil
+}
