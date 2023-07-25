5f4881d63df0da29b3acfe9e3a2e2ab277ad8ae9
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-25 15:59:50 -0800

    set up SearchAvailabilityByDates to return true if availability exists for roomID and false if no availability in postgres.go

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -62,3 +62,30 @@ updated_at)  values ($1, $2, $3, $4, $5, $6, $7)`
 	}
 	return nil
 }
+
+// SearchAvailabilityByDates returns true if availability exists for roomID and false if no availability
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `SELECT
+							count(id)
+						FROM
+    					room_restrictions
+						WHERE
+							room_id = $1
+    					AND $2 < end_date AND $3 > start_date`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
+}
