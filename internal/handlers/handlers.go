8e1806b6ed24e6cbfb4d31426dae7b7f1d83b6d5
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-25 18:04:51 -0800

    build ChooseRoom handler in handlers.go. Build route for ChooseRoom Build choose-room.page.tmpl

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/simpleittools/bookings/internal/config"
 	"github.com/simpleittools/bookings/internal/driver"
 	"github.com/simpleittools/bookings/internal/forms"
@@ -256,4 +257,24 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 			Data: data,
 		},
 	)
+}
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	res.RoomID = roomID
+
+	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
