c50ad82c9ae9e45c278de72c6e8d15fbd67b7631
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-29 11:23:07 -0800

    created book-room.js to contain the javascript for booking a room. Add the parameter of room to collect the room id from the specific page. in majors.page.tmpl added book-room.js and modify the "Check availability" to a button with an on-click of book_room(2) getting a json error. Continue to diagnose.

---
+++ static/js/book-room.js
@@ -0,0 +1,78 @@
+function book_room(room) {
+    document.getElementById("check-availability-button").addEventListener("click", function () {
+        let html = `
+        <form id="check-availability-form" action="" method="post" novalidate class="needs-validation">
+            <div class="form-row">
+                <div class="col">
+                    <div class="form-row" id="reservation-dates-modal">
+                        <div class="col">
+                            <input disabled required class="form-control" type="text" name="start" id="start" placeholder="Arrival">
+                        </div>
+                        <div class="col">
+                            <input disabled required class="form-control" type="text" name="end" id="end" placeholder="Departure">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        `;
+        attention.custom({
+            title: 'Choose your dates',
+            msg: html,
+            willOpen: () => {
+                const elem = document.getElementById("reservation-dates-modal");
+                const rp = new DateRangePicker(elem, {
+                    format: 'yyyy-mm-dd',
+                    showOnFocus: true,
+                    minDate: new Date(),
+                })
+            },
+            didOpen: () => {
+                document.getElementById("start").removeAttribute("disabled");
+                document.getElementById("end").removeAttribute("disabled");
+            },
+            preConfirm: () => {
+                return [
+                    document.getElementById('start').value,
+                    document.getElementById('end').value
+                ]
+            },
+            callback: function(result) {
+                console.log("called");
+
+                let form = document.getElementById("check-availability-form");
+                let formData = new FormData(form);
+                formData.append("csrf_token", "{{.CSRFToken}}");
+                formData.append("room_id", room);
+
+                fetch('/search-availability-json', {
+                    method: "post",
+                    body: formData,
+
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.ok) {
+                            attention.custom({
+                                icon: 'success',
+                                showConfirmButton: false,
+                                msg: '<p>Room is available!</p>' +
+                                    '<p><a href="/book-room?id=' +
+                                    data.room_id +
+                                    '&s=' +
+                                    data.start_date +
+                                    '&e=' +
+                                    data.end_date +
+                                    '" class="btn btn-primary">' +
+                                    'Book Now!</a></p>',
+                            })
+                        } else {
+                            attention.error({
+                                msg: "No Availability",
+                            })
+                        }
+                    })
+            }
+        });
+    })
+}
